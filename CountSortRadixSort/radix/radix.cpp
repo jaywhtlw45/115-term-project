d2e5bef48defe200827972ee70ece74443e14ebf
cdvcodes <126428932+cdvcodes@users.noreply.github.com>
2023-04-17 18:00:59 -0700

    Updated benchmarking to output avg. of 10 trials per case

--- CountSortRadixSort/radix/radix.cpp
+++ CountSortRadixSort/radix/radix.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
+#include <vector>
 #include <chrono>
 #include <time.h>
 #include <math.h>
@@ -47,7 +49,88 @@ void radix_sort(int* A, int n, int d) {
     }
 }
 
+void parse_line(std::string line, int* A, int n) {
+    std::stringstream ss;
+    ss << line;
+
+    int i = 0;
+    int t_num;
+    while (ss >> t_num) {
+        A[i] = t_num;
+        i++;
+    }
+}
+
+void gather_data(int* best, int* worst, int* avg, int n) {
+    std::string filename;
+    if (n == 10) filename = "ten.txt";
+    else if (n == 100) filename = "onehundred.txt";
+    else if (n == 1000) filename = "onethousand.txt";
+    else if (n == 10000) filename = "tenthousand.txt";
+    else if (n == 50000) filename = "fiftythousand.txt";
+    else filename = "onehundredthousand.txt";
+
+    std::ifstream my_file(filename);
+    std::string in;
+    int count = 0;
+
+    while (getline(my_file, in)) {
+        if (!count) parse_line(in, best, n);
+        else if (count == 1) parse_line(in, worst, n);
+        else parse_line(in, avg, n);
+
+        count++;
+    }
+}
+
+void benchmark_avg(int* A, int n, std::string sort) {
+    std::cout << "Running trials for " << sort << " case, n = " << n << '\n';
+    int trials = 10;
+    int time_taken = 0;
+    for (int i = 0; i < trials; i++) {
+        int max = get_max(A, n);
+        int digits = get_digits(max);
+        auto tick = std::chrono::high_resolution_clock::now();
+        radix_sort(A, n, digits);
+        auto tock = std::chrono::high_resolution_clock::now();
+        std::chrono::duration<double, std::micro> logged_time = (tock - tick);
+        time_taken += logged_time.count();
+    }
+
+    std::cout << "Results: " << (time_taken/trials) << " microseconds\n";
+    std::cout << "-------------------\n";
+}
+
 int main() {
-    srand(time(0));
+srand(time(0));
+    std::vector<int*> data;
     int data_sizes[6] = {10, 100, 1000, 10000, 50000, 100000};
+
+    for (auto &size: data_sizes) {
+        int* t_best = new int[size];
+        int* t_worst = new int[size];
+        int* t_avg = new int[size];
+
+        gather_data(t_best, t_worst, t_avg, size);
+
+        data.push_back(t_best);
+        data.push_back(t_worst);
+        data.push_back(t_avg);
+    }
+
+    int i = -1;
+    for (int j = 0; j < data.size(); j++) {
+        std::string sort;
+        int mod = j % 3;
+        if (!mod) {
+            sort = "best";
+            i++;
+        }
+        else if (mod == 1) sort = "worst";
+        else if (mod == 2) {
+            sort = "avg";
+        }
+
+        benchmark_avg(data[j], data_sizes[i], sort);
+    }
 }
