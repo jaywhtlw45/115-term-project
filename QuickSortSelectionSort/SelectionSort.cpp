6e874dca1a1117cd7caf88dd7e0b280cdd5073f8
chrismckie19 <130183430+chrismckie19@users.noreply.github.com>
2023-04-24 15:28:03 -0700

    Updated to make runtimes more consistent

--- QuickSortSelectionSort/SelectionSort.cpp
+++ QuickSortSelectionSort/SelectionSort.cpp
@@ -17,6 +17,14 @@ void swap(int & x, int & y){
     y = temp;
 }
 
+//Prints the sorted data set. Mainly used for testing purposes
+void printData(int* array, int size) {
+    for(int i = 0; i < size; i++) {
+        cout << array[i] << " ";
+    }
+    cout << endl;
+}
+
 //Selection Sort algorithm
 void selectionSort(int* array, int size) {
     for(int i = 0; i < size; i++) {
@@ -30,74 +38,89 @@ void selectionSort(int* array, int size) {
     }
 }
 
-//Calls and times Selection Sort.
-void clock(int* array, int size) {
-    long sum = 0;
-    for(int i = 0; i < 10; i++){
-        auto start = high_resolution_clock::now();
-        selectionSort(array, size);
-        auto stop = high_resolution_clock::now();
-        auto duration = duration_cast<nanoseconds>(stop - start);
-        sum += duration.count();
-        //cout << "Set " << i + 1 << ": " << duration.count() << "nanoseconds" << endl;
-    }
-    cout << "Array Length: " << size << " elements." << endl;
-    cout << "Average Time (out of 10): " << sum / 10 << " nanoseconds" << endl;
-}
-
-//Prints the sorted data set
-void printData(int* array, int size) {
-    for(int i = 0; i < size; i++) {
-        cout << array[i] << " ";
-    }
-    cout << endl;
+//Calls and times Selection Sort. Stop time - start time = duration
+long clock(int* array, int size) {
+    auto start = high_resolution_clock::now();
+    selectionSort(array, size);
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<nanoseconds>(stop - start);
+    long clockTime = duration.count();
+    
+    return clockTime;
 }
 
 //Reads each file into program and clocks Selection Sort at different best case, worst case, and average case
 void runSort(ifstream & txtFile, string fileName, int size) {
-    //Opens the text file based on input
-    txtFile.open(fileName);
-    if (!txtFile.is_open()) {
-        cout << "Could not open file " << fileName << endl;
-    } else {
-        
-        //Arrays to store the data from the file
-        int* bestCase = new int[size];
-        int* worstCase = new int[size];
-        int* avgCase = new int[size];
-        
-        //Reads the text files into each of the arrays
-        for(int i = 0; i < size; i++) {
-            txtFile >> bestCase[i];
+    //Arrays to store the data from the file
+    int* bestCase = new int[size];
+    int* worstCase = new int[size];
+    int* avgCase = new int[size];
+    
+    //Variables to store the cumulative sums of algorithm run times
+    long sumBest = 0;
+    long sumWorst = 0;
+    long sumAvg = 0;
+
+    for(int i = 0; i < 15; i++) {
+        //Opens the text file based on string input
+        txtFile.open(fileName);
+        if (!txtFile.is_open()) {
+            cout << "Could not open file " << fileName << endl;
+        } else {
+            //Reads the text files into each of the arrays
+            for(int i = 0; i < size; i++) {
+                txtFile >> bestCase[i];
+            }
+            for(int i = 0; i < size; i++) {
+                txtFile >> worstCase[i];
+            }
+            for(int i = 0; i < size; i++) {
+                txtFile >> avgCase[i];
+            }
+            txtFile.close();
+            
+            //Runs the clock function on each of the arrays
+            //First 5 runs are ignored due to inconsistency. Results even out after the first several runs
+            //Runs 6-15 are saved to cumulative sum for the average clock time
+            long currBest = clock(bestCase, size);
+            if(i >= 5) {
+                sumBest += currBest;
+            };
+            long currWorst = clock(worstCase, size);
+            if(i >= 5) {
+                sumWorst += currWorst;
+            };
+            long currAvg = clock(avgCase, size);
+            if(i >= 5) {
+                sumAvg += currAvg;
+            };
         }
-        for(int i = 0; i < size; i++) {
-            txtFile >> worstCase[i];
-        }
-        for(int i = 0; i < size; i++) {
-            txtFile >> avgCase[i];
-        }
-        txtFile.close();
-        
-        //Runs the clock function on each of the arrays
-        cout << "Best Case - Selection Sort:" << endl;
-        clock(bestCase, size);
-        cout << endl;
-        cout << "Worst Case - Selection Sort:" << endl;
-        clock(worstCase, size);
-        cout << endl;
-        cout << "Average Case - Selection Sort:" << endl;
-        clock(avgCase, size);
-        cout << endl << endl;
-        
-        delete[] bestCase;
-        delete[] worstCase;
-        delete[] avgCase;
     }
+    // Output the best case results
+    cout << "Best Case - Selection Sort " << size << " elements:" << endl;
+    cout << "Average Time (out of 10): " << sumBest / 10 << " nanoseconds" << endl;
+    cout << endl;
+    
+    // Output the worst case results
+    cout << "Worst Case - Selection Sort " << size << " elements:" <<endl;
+    cout << "Average Time (out of 10): " << sumWorst / 10 << " nanoseconds" << endl;
+    cout << endl;
+    
+    // Output the avg case results
+    cout << "Average Case - Selection Sort " << size << " elements:" <<endl;
+    cout << "Average Time (out of 10): " << sumAvg / 10 << " nanoseconds" << endl;
+    cout << endl << endl << endl;
+    
+    delete[] bestCase;
+    delete[] worstCase;
+    delete[] avgCase;
 }
 
 int main() {
     ifstream txtFile;
     
+    //Calculates the average run time for best, worst, and average cases of Selection Sort.
+    //Computes the run times with data sets of size 10, 100, 1000, 50000, and 100000
     runSort(txtFile, "ten.txt", 10);
     runSort(txtFile, "onehundred.txt", 100);
     runSort(txtFile, "onethousand.txt", 1000);
@@ -105,9 +128,5 @@ int main() {
     runSort(txtFile, "fiftythousand.txt", 50000);
     runSort(txtFile, "onehundredthousand.txt", 100000);
 
-    //printData(bestCase, size);
-    //printData(worstCase, size);
-    //printData(avgCase, size);
-    
     return 0;
 }
