2de5b86eb45a81da8f195e0ae2927cf705844da3
chrismckie19 <130183430+chrismckie19@users.noreply.github.com>
2023-04-27 23:31:17 -0700

    Same algorithms but tracks comparisons

---
+++ QuickSortSelectionSort/QuickSortComps.cpp
@@ -0,0 +1,207 @@
+// CSCI 115 Project
+// Quick Sort
+// Chris McKie
+
+#include <iostream>
+#include <string>
+#include <chrono>
+#include <fstream>
+
+using namespace std;
+using namespace std::chrono;
+
+//Swaps two values
+void swap(int & x, int & y){
+    int temp = x;
+    x = y;
+    y = temp;
+}
+
+//Prints the sorted data set. Mainly used for testing purposes
+void printData(int* array, int size) {
+    for(int i = 0; i < size; i++) {
+        cout << array[i] << " ";
+    }
+    cout << endl;
+}
+
+//Partition used in quick sort. Uses first element in the array as a pivot
+int partition(int* array, int low, int high, long & comparisons) {
+    int pivot = array[low];
+    int i = low - 1;
+    int j = high + 1;
+    comparisons++;
+    while(i < j) {
+        do {
+            i++;
+            comparisons++;
+        } while(array[i] < pivot);
+        do {
+            j--;
+            comparisons++;
+        } while(array[j] > pivot);
+        comparisons++;
+        if(i < j) {
+            swap(array[i], array[j]);
+        }
+    }
+    swap(array[low], array[j]);
+    return j;
+}
+
+//Quick Sort algorithm. Uses first element in the array as a pivot
+void quickSort(int* array, int p, int r, long & comparisons) {
+    comparisons++;
+    if(p < r) {
+        int q = partition(array, p, r, comparisons);
+        quickSort(array, p, q, comparisons);
+        quickSort(array, (q + 1), r, comparisons);
+    }
+}
+
+//Calls and times Quick Sort. Uses first pivot for average and worst case. Stop time - start time = duration
+long clock(int* array, int size, long & comparisons) {
+    auto start = high_resolution_clock::now();
+    quickSort(array, 0, (size - 1), comparisons);
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<nanoseconds>(stop - start);
+    long clockTime = duration.count();
+    
+    return clockTime;
+}
+
+//Partition used in quick sort. Uses the middle element as a pivot, giving the best case scenario on an already sorted array
+int partitionBestCase(int* array, int low, int high, long & comparisons) {
+    int mid = (high - low)/2 + low;
+    int pivot = array[mid];
+    int i = low - 1;
+    int j = high + 1;
+    comparisons++;
+    while(i < j) {
+        do {
+            i++;
+            comparisons++;
+        } while(array[i] < pivot);
+        do {
+            j--;
+            comparisons++;
+        } while(array[j] > pivot);
+        comparisons++;
+        if(i < j) {
+            swap(array[i], array[j]);
+        }
+    }
+    swap(array[low], array[j]);
+    return j;
+}
+
+//Quick Sort algorithm. Uses the middle element as a pivot, giving the best case scenario on an already sorted array
+void quickSortBestCase(int* array, int p, int r, long & comparisons) {
+    comparisons++;
+    if(p < r) {
+        int q = partitionBestCase(array, p, r, comparisons);
+        quickSortBestCase(array, p, q, comparisons);
+        quickSortBestCase(array, (q + 1), r, comparisons);
+    }
+}
+
+//Calls and times Quick Sort. Uses the middle element as a pivot, giving the best case scenario on an already sorted array. Stop time - start time = duration
+long clockBestCase(int* array, int size, long & comparisons) {
+    auto start = high_resolution_clock::now();
+    quickSortBestCase(array, 0, (size - 1), comparisons);
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<nanoseconds>(stop - start);
+    long clockTime = duration.count();
+    
+    return clockTime;
+}
+
+//Reads each file into program and clocks Quick Sort at different best case, worst case, and average case
+void runSort(ifstream & txtFile, string fileName, int size) {
+    //Arrays to store the data from the file
+    int* bestCase = new int[size];
+    int* worstCase = new int[size];
+    int* avgCase = new int[size];
+    
+    //Variables to store the cumulative sums of algorithm run times
+    long sumBest = 0;
+    long sumWorst = 0;
+    long sumAvg = 0;
+    
+    //Variables to store the cumulative number of comparisons
+    long comparesB = 0;
+    long comparesW = 0;
+    long comparesA = 0;
+
+    for(int i = 0; i < 15; i++) {
+        //Opens the text file based on string input
+        txtFile.open(fileName);
+        if (!txtFile.is_open()) {
+            cout << "Could not open file " << fileName << endl;
+        } else {
+            //Reads the text files into each of the arrays
+            for(int i = 0; i < size; i++) {
+                txtFile >> bestCase[i];
+            }
+            for(int i = 0; i < size; i++) {
+                txtFile >> worstCase[i];
+            }
+            for(int i = 0; i < size; i++) {
+                txtFile >> avgCase[i];
+            }
+            txtFile.close();
+            
+            //Runs the clock function on each of the arrays
+            //First 5 runs are ignored due to inconsistency. Results even out after the first several runs
+            //Runs 6-15 are saved to cumulative sum for the average clock time
+            long currBest = clockBestCase(bestCase, size, comparesB);
+            if(i >= 5) {
+                sumBest += currBest;
+            };
+            long currWorst = clock(worstCase, size, comparesW);
+            if(i >= 5) {
+                sumWorst += currWorst;
+            };
+            long currAvg = clock(avgCase, size, comparesA);
+            if(i >= 5) {
+                sumAvg += currAvg;
+            };
+        }
+    }
+    // Output the best case results
+    cout << "Best Case - Quick Sort " << size << " elements:" << endl;
+    cout << "Average Time (out of 10): " << sumBest / 10 << " nanoseconds" << endl;
+    cout << "Number of Comparisons: " << comparesB / 10 << endl;
+    cout << endl;
+    
+    // Output the worst case results
+    cout << "Worst Case - Quick Sort " << size << " elements:" <<endl;
+    cout << "Average Time (out of 10): " << sumWorst / 10 << " nanoseconds" << endl;
+    cout << "Number of Comparisons: " << comparesW / 10 << endl;
+    cout << endl;
+    
+    // Output the avg case results
+    cout << "Average Case - Quick Sort " << size << " elements:" <<endl;
+    cout << "Average Time (out of 10): " << sumAvg / 10 << " nanoseconds" << endl;
+    cout << "Number of Comparisons: " << comparesA / 10 << endl;
+    cout << endl << endl << endl;
+    
+    delete[] bestCase;
+    delete[] worstCase;
+    delete[] avgCase;
+}
+
+int main() {
+    ifstream txtFile;
+    
+    //Calculates the average run time for best, worst, and average cases of Quick Sort.
+    //Computes the run times with data sets of size 10, 100, 1000, 50000, and 100000
+    runSort(txtFile, "ten.txt", 10);
+    runSort(txtFile, "onehundred.txt", 100);
+    runSort(txtFile, "onethousand.txt", 1000);
+    runSort(txtFile, "tenthousand.txt", 10000);
+    runSort(txtFile, "fiftythousand.txt", 50000);
+    runSort(txtFile, "onehundredthousand.txt", 100000);
+
+    return 0;
+}
---
+++ QuickSortSelectionSort/SelectionSortComps.cpp
@@ -0,0 +1,137 @@
+// CSCI 115 Project
+// Selection Sort with comparisons
+// Chris McKie
+
+#include <iostream>
+#include <string>
+#include <chrono>
+#include <fstream>
+
+using namespace std;
+using namespace std::chrono;
+
+//Swaps two values
+void swap(int & x, int & y){
+    int temp = x;
+    x = y;
+    y = temp;
+}
+
+//Prints the sorted data set. Mainly used for testing purposes
+void printData(int* array, int size) {
+    for(int i = 0; i < size; i++) {
+        cout << array[i] << " ";
+    }
+    cout << endl;
+}
+
+//Selection Sort algorithm
+void selectionSort(int* array, int size, long & comparisons) {
+    for(int i = 0; i < size; i++) {
+        int min = i;
+        for(int j = min + 1; j < size; j++) {
+            comparisons++;
+            if(array[j] <  array[min]) {
+                min = j;
+            }
+        }
+        swap(array[min], array[i]);
+    }
+}
+
+//Calls and times Selection Sort. Stop time - start time = duration
+long clock(int* array, int size, long & comparisons) {
+    auto start = high_resolution_clock::now();
+    selectionSort(array, size, comparisons);
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<nanoseconds>(stop - start);
+    long clockTime = duration.count();
+    
+    return clockTime;
+}
+
+//Reads each file into program and clocks Selection Sort at different best case, worst case, and average case
+void runSort(ifstream & txtFile, string fileName, int size) {
+    //Arrays to store the data from the file
+    int* bestCase = new int[size];
+    int* worstCase = new int[size];
+    int* avgCase = new int[size];
+    
+    //Variables to store the cumulative sums of algorithm run times
+    long sumBest = 0;
+    long sumWorst = 0;
+    long sumAvg = 0;
+    
+    //Variables to store the cumulative number of comparisons
+    long comparesB = 0;
+    long comparesW = 0;
+    long comparesA = 0;
+
+    for(int i = 0; i < 15; i++) {
+        //Opens the text file based on string input
+        txtFile.open(fileName);
+        if (!txtFile.is_open()) {
+            cout << "Could not open file " << fileName << endl;
+        } else {
+            //Reads the text files into each of the arrays
+            for(int i = 0; i < size; i++) {
+                txtFile >> bestCase[i];
+            }
+            for(int i = 0; i < size; i++) {
+                txtFile >> worstCase[i];
+            }
+            for(int i = 0; i < size; i++) {
+                txtFile >> avgCase[i];
+            }
+            txtFile.close();
+            
+            //Runs the clock function on each of the arrays
+            long currBest = clock(bestCase, size, comparesB);
+            long currWorst = clock(worstCase, size, comparesW);
+            long currAvg = clock(avgCase, size, comparesA);
+            //First 5 runs are ignored due to inconsistency. Results even out after the first several runs
+            //Runs 6-15 are saved to cumulative sum for the average clock time
+            if(i >= 5) {
+                sumBest += currBest;
+                sumWorst += currWorst;
+                sumAvg += currAvg;
+            };
+        }
+    }
+    // Output the best case results
+    cout << "Best Case - Selection Sort " << size << " elements:" << endl;
+    cout << "Average Time (out of 10): " << sumBest / 10 << " nanoseconds" << endl;
+    cout << "Number of Comparisons: " << comparesB / 10 << endl;
+    cout << endl;
+    
+    // Output the worst case results
+    cout << "Worst Case - Selection Sort " << size << " elements:" <<endl;
+    cout << "Average Time (out of 10): " << sumWorst / 10 << " nanoseconds" << endl;
+    cout << "Number of Comparisons: " << comparesW / 10 << endl;
+    cout << endl;
+    
+    // Output the avg case results
+    cout << "Average Case - Selection Sort " << size << " elements:" <<endl;
+    cout << "Average Time (out of 10): " << sumAvg / 10 << " nanoseconds" << endl;
+    cout << "Number of Comparisons: " << comparesA / 10 << endl;
+    cout << endl << endl << endl;
+    
+    delete[] bestCase;
+    delete[] worstCase;
+    delete[] avgCase;
+}
+
+int main() {
+    ifstream txtFile;
+    
+    //Calculates the average run time for best, worst, and average cases of Selection Sort.
+    //Computes the run times with data sets of size 10, 100, 1000, 50000, and 100000
+    runSort(txtFile, "ten.txt", 10);
+    runSort(txtFile, "onehundred.txt", 100);
+    runSort(txtFile, "onethousand.txt", 1000);
+    runSort(txtFile, "tenthousand.txt", 10000);
+    runSort(txtFile, "fiftythousand.txt", 50000);
+    runSort(txtFile, "onehundredthousand.txt", 100000);
+
+    return 0;
+}
